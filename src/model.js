6fe56055d83d01b59c35a9672c03f63507fce44b
Alexander Buiko <reed665@hotmail.com>
2018-05-22 16:47:31 +0300

    add cards view functions

--- src/model.js
+++ src/model.js
@@ -1,3 +1,20 @@
 export default {
-  foo: 'bar',
+  cards: [
+    {
+      question: 'question #1',
+      answer: 'answer #1',
+    },
+    {
+      question: 'question #2',
+      answer: 'answer #2',
+    },
+    {
+      question: 'question #3',
+      answer: 'answer #3',
+    },
+    {
+      question: 'question #4',
+      answer: 'answer #4',
+    },
+  ]
 }
--- src/view.js
+++ src/view.js
@@ -1,11 +1,35 @@
 import hh from 'hyperscript-helpers'
 import { h } from 'virtual-dom'
 
-const { div, h1, pre } = hh(h)
+const { div, h1, pre, a, i, span } = hh(h)
+
+function addButton(dispatch) {
+  return a({ className: 'f4 link dim ph3 pv2 mb3 ml2 dib white bg-green'}, [
+    i({ className: 'fa fa-plus mr2' }),
+    span('Add Flashcard'),
+  ])
+}
+
+function cardItem(dispatch, card) {
+  return div({ className: 'bg-light-yellow relative shadow-4 w-30 pa3 ma2' }, [
+    i({ className: 'fa fa-times black-60 absolute top-1 right-1' }),
+    div({ className: 'f5 b underline mv1' }, 'Question'),
+    div(card.question),
+    div({ className: 'f5 b underline mt3 mb1' }, 'Answer'),
+    div(card.answer),
+  ])
+}
+
+function cardList(dispatch, model) {
+  const cards = model.cards.map(card => cardItem(dispatch, card))
+  return div({ className: 'flex flex-wrap' }, cards)
+}
 
 export default function view(dispatch, model) {
-  return div({ className: 'mw6 center' }, [
-    h1({ className: 'f2 pv2 bb' }, 'Flashcards' ),
+  return div({ className: 'mw8 center' }, [
+    h1({ className: 'f2 pv2 bb' }, 'Flashcards'),
+    addButton(dispatch),
+    cardList(dispatch, model),
     pre(JSON.stringify(model, null, 2)),
   ])
 }
